0dcf7331cb2fec28a9102e2467a9767b02d68cc7
Rodelph <anaqi@um6ss.ma>
2021-09-19 21:42:58 +0100

    first commit

---
+++ ImageProc/src/ImageProc/header/imgproc.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <dcmtk/dcmimgle/dcmimage.h>
+
+#include <boost/align.hpp>
+
+class imageproc
+{
+    public:
+
+
+    private:
+
+};
---
+++ ImageProc/src/ImageProc/imgproc.cpp
@@ -0,0 +1,2 @@
+#include "header/imgproc.hpp"
+

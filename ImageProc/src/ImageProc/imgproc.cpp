503e39e884b53ffcc8b4c5ef38f485876142cd39
rodelph <anaqi@um6ss.ma>
2021-09-23 00:13:14 +0100

    more dcm stuff

--- ImageProc/src/ImageProc/imgproc.cpp
+++ ImageProc/src/ImageProc/imgproc.cpp
@@ -11,12 +11,13 @@ imgproc::~imgproc() { delete ui; }
 
 void imgproc::on_testBtn_clicked()
 {
-    DicomImage *image = new DicomImage("../../res/1.dcm");
+    DicomImage *image = new DicomImage("../../resDcm/1-002.dcm");
     image->setMinMaxWindow();
     cv::Mat dst;
     cv::Mat inputImage(uint16_t(image->getHeight()), uint16_t(image->getWidth()), CV_16UC1, (uint16_t*)image->getOutputData(16));
     cv::medianBlur(inputImage, dst, 3);
     cv::imshow("image", dst);
+    cv::imwrite("../../resPng/1-001.png", dst);
     cv::waitKey();
     cv::destroyAllWindows();
 }
